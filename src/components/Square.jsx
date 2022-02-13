eca121ac86b02be69ee96ab2f5b8997d211412f3
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-14 00:35:24 +0530

    porps work done

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Square() {
-  return <button></button>;
+function Square(props) {
+  return <button type="button">{props.value}</button>;
 }
 export default Square;
