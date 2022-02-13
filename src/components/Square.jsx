870654cf6b93d744e8ac26f49904edb910b2d250
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-14 00:40:33 +0530

    short use of props

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Square(props) {
-  return <button type="button">{props.value}</button>;
+function Square(value) {
+  return <button type="button">{value}</button>;
 }
 export default Square;
