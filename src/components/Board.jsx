952fcbd33c195e14142e4119d6a43a7d46d2cc4c
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-14 15:52:24 +0530

    More simplified squares

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,22 +5,37 @@ function Board() {
   const [board, setBoard] = useState(Array(9).fill(null));
   console.log(board);
 
+  function handleBoard() {
+    console.log("hYY");
+  }
+
+  const renderSquare = (position) => {
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => {
+          handleBoard(position);
+        }}
+      />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={board[0]} />
-        <Square value={board[1]} />
-        <Square value={board[2]} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={board[3]} />
-        <Square value={board[4]} />
-        <Square value={board[5]} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(6)}
       </div>
       <div className="board-row">
-        <Square value={board[6]} />
-        <Square value={board[7]} />
-        <Square value={board[8]} />
+        {renderSquare(7)}
+        {renderSquare(8)}
+        {renderSquare(9)}
       </div>
     </div>
   );
