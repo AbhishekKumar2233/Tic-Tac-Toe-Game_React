f1be80d0e1bded1665aa9d20ab6769df72c4ca7d
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-15 13:59:14 +0530

    match tied logic completed

--- src/components/StatusMessage.jsx
+++ src/components/StatusMessage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 export default function StatusMessage({ winner, current }) {
+  const noMoveleft = current.board.every((el) => el !== null);
+
   return (
     <h2>
       {winner && `Winner is ${winner}`}
-      {!winner && `Next Player is ${current.isXNext ? "X" : "O"}`}
+      {!winner &&
+        !noMoveleft &&
+        `Next Player is ${current.isXNext ? "X" : "O"}`}
+      {!winner && noMoveleft && "X and O tied"}
     </h2>
   );
 }
